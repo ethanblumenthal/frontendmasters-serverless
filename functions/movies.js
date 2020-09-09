7c651252f6839659a1ecd3361aeae84f49d433ce
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-09-09 16:06:06 -0500

    Setup GraphQL API using Hasura for data.

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,44 @@
-const movies = require('../data/movies.json');
+const { URL } = require('url');
+const fetch = require('node-fetch');
+const { query } = require('./util/hasura');
 
 exports.handler = async () => {
+  const { movies } = await query({
+    query: `
+      query {
+        movies {
+          id
+          title
+          tagline
+          poster
+        }
+      }
+    `,
+  });
+
+  const api = new URL('https://www.omdbapi.com/');
+
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    api.searchParams.set('i', movie.id);
+
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies),
+    body: JSON.stringify(moviesWithRatings),
   };
 };
